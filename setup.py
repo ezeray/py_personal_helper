e7608a1fa294cee9ff6344d26736485d9e290993
Ezequiel Raigorodsky <ezequielraigorodsky@gmail.com>
2022-11-20 18:13:44 -0300

    feature/setup-as-installable-package
    created setup.py file

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+Setuptools setup script.
+"""
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="py_personal_helper",
+    version="0.1.0",
+    description="Ezequiel Raigorodsky's personal helper package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ezeray/py_personal_helper",
+    author="Ezequiel Raigorodsky",
+    author_email="ezequielraigorodsky@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Helper",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="development, helper, utils",
+    packages_dir={"": "personal_helper"},
+    packages=find_packages(where="personal_helper"),
+    python_requires=">=3.9, <4",
+    # install_requires=[],
+)
